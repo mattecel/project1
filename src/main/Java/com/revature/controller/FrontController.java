16555a3981cc54ea4019e4cf8cc2a6b44a042619
mattecel <matteck123@gmail.com>
2021-06-18 18:34:08 -0400

    beginning to connect

--- src/main/Java/com/revature/controller/FrontController.java
+++ src/main/Java/com/revature/controller/FrontController.java
@@ -13,63 +13,69 @@ import com.revature.facilities.AuthorService;
 import com.revature.facilities.AuthorServiceImpl;
 
 public class FrontController extends HttpServlet {
-	
+
 	private AuthorService auths = new AuthorServiceImpl();
 	private Gson gson = new Gson();
-	
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		class AuthorCred {
+			public String user;
+			public String pass;
+		}
 		
 		
 		String uri = request.getRequestURI();
-		
-		System.out.println(uri);
+		String loggedId;
 
-		/* for GET requests:
+		/*
+		 * for GET requests:
 		 * 
-		 *  localhost:8080/CatAppServlet/cats -> get all cats
-		 *  
-		 *  /cats/<id> -> get a cat by id
-		 *  
-		 *  for POST requests:
-		 *  /cats -> add cat
-		 *  
-		 *  */
+		 * localhost:8080/CatAppServlet/cats -> get all cats
+		 * 
+		 * /cats/<id> -> get a cat by id
+		 * 
+		 * for POST requests: /cats -> add cat
+		 * 
+		 */
 		switch (uri) {
 
 		case "/project1/author": {
-			
-			if (request.getMethod() == "GET") {
+
+			if (request.getMethod().equals("GET")) {
 				System.out.println("Getting author....");
 				Author author = auths.getAuthor("lulu1", "123");
 				System.out.println(author);
-				response.setHeader("Access-Control-Allow-Origin","*");
+				response.setHeader("Access-Control-Allow-Origin", "*");
 				response.getWriter().append(gson.toJson(author));
+
 				break;
 			} else {
 				System.out.println("Adding author...");
 				Author author = auths.getAuthor("lulu1", "123");
 				System.out.println(author);
-				//response.setHeader("Access-Control-Allow-Origin","*");
-				response.setHeader("Origin", "http://localhost:8080/project1/author");
+				// response.setHeader("Access-Control-Allow-Origin","*");
+				response.setHeader("Origin", "http://localhost:8080/project1/author/");
 				response.getWriter().append(gson.toJson(author));
 				break;
 			}
-			
+
 		}
-		
+		case ("/project1/author-login"): {
+			AuthorCred aci = this.gson.fromJson(request.getReader(), AuthorCred.class);
+			Author a = auths.getAuthor(aci.user, aci.pass);
+			session.setAttribute("logged_in", a)
+		}
+
 		default: {
 			System.out.println("Reached the default case...");
 			response.sendError(418, "BRB MAKING TEA");
 		}
-		
-		
+
 		}
-		
-		
-		
+
 	}
-	
+
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.getWriter().append("Post request");
--- src/main/webapp/AuthorLogin.js
+++ src/main/webapp/AuthorLogin.js
@@ -13,8 +13,86 @@
 let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
 // Add event listener
-document.getElementById('login-button').onclick = login;
+document.getElementById('login-button').addEventListener('click', login);
+let dataSection = document.getElementById('author-data');
+
 
 function login() {
-    console.log("im gay");
+    console.log("button is clicked");
+
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password');
+
+    let url = apiUrl + username;
+
+    // AJAX
+
+    // Step 1 Create an XMLHHttpRequestObject
+    let xhttp = new XMLHttpRequest();
+
+    // Step 2 Set a callback function for the readystatechange event
+    xhttp.onreadystatechange = recieveData;
+
+    // Step 3 Open the request
+    xhttp.open('GET', url, true);
+
+    // Step 4: Send the request
+    xhttp.send();
+
+    
+
+    // callback function
+    function recieveData(){
+        /**
+         * 
+         * 
+         * 
+         * 
+         * 
+         * */
+        // Get the element we want to manipulate
+
+        
+        dataSection.innerHTML = '';
+
+        if (xhttp.readyState == 4) {
+            if (xhttp.status == 200) {
+
+                let r = xhttp.responseText;
+
+                r = JSON.parse(r);
+                console.log(r + "This is the results object");
+
+                //Populate page with data
+                populateData(r)
+
+
+            } else {
+                // Ready state is done but status code is not "Ok"
+            }
+        } else {
+            // error handling
+        }
+    }
+}
+
+const populateData = (res) => {
+    // name
+    let nameTag = document.createElement('h3');
+    nameTag.innerHTML = res.name;
+    dataSection.appendChild(nameTag);
+    // abilities
+    let abilitiesArray = res.abilities;
+    console.log(abilitiesArray);
+
+    let abilities = document.createElement('ul');
+    dataSection.innerHTML += 'Abilities<br>';
+    dataSection.appendChild(abilities);
+
+    abilitiesArray.map(a => {
+        ablit = document.createElement("li")
+        console.log(a);
+        ablit.innerHTML = a.ability.name;
+        abilities.appendChild(ablit);
+    })
 }
